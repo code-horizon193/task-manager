20dc1236e9a0891e97242fbd43b0e76d68ce37c6
tarek fawzy <tf5234045@gmail.com>
2025-05-07 16:46:04 +0300

    change TeamCard colors

--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -3,7 +3,7 @@ import { BsFacebook, BsLinkedin, BsTwitterX } from 'react-icons/bs';
 
 const TeamCard = ({ name, image, about, position }) => {
     return (
-        <div className='p-3 rounded-lg bg-slate-100 dark:bg-slate-900 border border-slate-300 dark:border-slate-600 transition-all duration-200 shadow-md shadow-blue-300/20 hover:shadow-sm'>
+        <div className='p-3 rounded-lg bg-slate-50/20 dark:bg-darkmode border border-slate-300 dark:border-slate-600 transition-all duration-200 shadow-md shadow-blue-300/20 hover:shadow-sm'>
             <div>
                 <div className="flex items-start">
                     <div className="size-14 rounded-full">
@@ -15,7 +15,7 @@ const TeamCard = ({ name, image, about, position }) => {
                     </div>
                     <div className="ml-3.5">
                         <p className="font-bold text-slate-600 dark:text-slate-300 capitalize text-lg">{name}</p>
-                        <div className="text-sm text-blue-500">     {position}
+                        <div className="text-sm text-orange-500">     {position}
                         </div>
                     </div>
                 </div>
@@ -25,13 +25,13 @@ const TeamCard = ({ name, image, about, position }) => {
 
                 <div className="mt-3 pt-3">
                     <div className="flex items-center justify-center gap-2">
-                        <span className="size-9 grid place-items-center border-slate-200 dark:border-slate-700 border rounded-full text-lg transition-all duration-200 hover:text-white hover:bg-blue-600 text-slate-600 dark:text-slate-300 cursor-pointer">
+                        <span className="size-9 grid place-items-center border-slate-200 dark:border-slate-700 border rounded-full text-lg transition-all duration-200 hover:text-white hover:bg-orange-600 text-slate-600 dark:text-slate-300 cursor-pointer">
                             <BsFacebook />
                         </span>
-                        <span className="size-9 grid place-items-center border-slate-200 dark:border-slate-700 border rounded-full text-lg transition-all duration-200 hover:text-white hover:bg-blue-600 text-slate-600 dark:text-slate-300 cursor-pointer">
+                        <span className="size-9 grid place-items-center border-slate-200 dark:border-slate-700 border rounded-full text-lg transition-all duration-200 hover:text-white hover:bg-orange-600 text-slate-600 dark:text-slate-300 cursor-pointer">
                             <BsLinkedin />
                         </span>
-                        <span className="size-9 grid place-items-center border-slate-200 dark:border-slate-700 border rounded-full text-lg transition-all duration-200 hover:text-white hover:bg-blue-600 text-slate-600 dark:text-slate-300 cursor-pointer">
+                        <span className="size-9 grid place-items-center border-slate-200 dark:border-slate-700 border rounded-full text-lg transition-all duration-200 hover:text-white hover:bg-orange-600 text-slate-600 dark:text-slate-300 cursor-pointer">
                             <BsTwitterX />
                         </span>
                     </div>
